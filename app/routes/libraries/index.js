dac5a336892ac34d81fd2c203dece1f4e5284e9c
Chuck Rice <charles.rice92@gmail.com>
2017-07-31 20:50:48 +0100

    lesson 4 complete but missing homework

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -4,6 +4,14 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('library');
+  },
+
+  actions: {
+
+    deleteLibrary(library) {
+      let confirmation = confirm(`Are you sure you want to delete ${library.get('name')}?`)
+      confirmation ? library.destroyRecord() : {}
+    }
   }
 
 });
