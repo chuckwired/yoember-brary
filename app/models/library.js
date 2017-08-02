0ea6f102d65c6ad6f9377497ba0a99bed1e51cfb
Charles Rice <charles.rice92@gmail.com>
2017-08-02 13:36:32 +0100

    lesson 5 completed

--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  // attribute properties
   name: DS.attr('string'),
   address: DS.attr('string'),
-  phone: DS.attr('string')
+  phone: DS.attr('string'),
+
+  // validation properties
+  isValid: Ember.computed.notEmpty('name')
 });
--- app/routes/libraries/edit.js
+++ app/routes/libraries/edit.js
@@ -6,6 +6,17 @@ export default Ember.Route.extend({
     return this.store.findRecord('library', params.library_id)
   },
 
+  setupController: function (controller, model) {
+    this._super(controller, model)
+
+    controller.set('title', `Edit ${model.get('name')}`)
+    controller.set('buttonLabel', 'Save changes')
+  },
+
+  renderTemplate() {
+    this.render('libraries/form')
+  },
+
   actions: {
 
     saveLibrary(library) {
